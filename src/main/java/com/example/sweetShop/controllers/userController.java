7a1dd2546f56ae591fa3cb82faf3d7deac7b49bd
Vinay13 <chintalapativinay813@gmail.com>
2025-12-13 23:09:32 +0530

    user controller created successfully

--- src/main/java/com/example/sweetShop/controllers/userController.java
+++ src/main/java/com/example/sweetShop/controllers/userController.java
@@ -1,9 +1,40 @@
 package com.example.sweetShop.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.sweetShop.models.Sweet;
+import com.example.sweetShop.repos.Sweetrepo;
+import com.example.sweetShop.repos.userRepo;
+import com.example.sweetShop.models.User;
+
+@RestController
+@RequestMapping("api/users")
 public class userController {
 
-	public userController() {
-		// TODO Auto-generated constructor stub
+	
+		
+		userRepo userrepo;
+		
+		public userController(userRepo userrepo) {
+			super();
+			this.userrepo = userrepo;
+		}
+		@PostMapping("create")
+		private String userCreate(@RequestBody User user){
+			
+			userrepo.save(user);
+			
+			return "user created";
+		}
+		
 	}
 
-}
+
+
+
+
+
+
